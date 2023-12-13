e30807c959fb22dfec0a91c1c795378fc9514716
NOVO Steeven <steeven_yugo@hotmail.fr>
2023-12-13 20:30:38 +0100

    save

--- projetnosql/projetnosql/urls.py
+++ projetnosql/projetnosql/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import add_book, book_list, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('add_book/', add_book, name='add_book'),
+    path('book_list/', book_list, name='book_list'),
 ]
